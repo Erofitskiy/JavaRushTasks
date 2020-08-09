79148fdbea71d23dcf32e19b092655a48901e77e
MRXover <14204072+MRXover@users.noreply.github.com>
2020-08-09 20:05:26 +0300

    09.08.2020

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1502/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task15.task1502;
+
+/* 
+ООП - Наследование животных
+*/
+
+public class Solution {
+    //добавьте классы Goose и Dragon тут
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class BigAnimal {
+        protected String getSize() {
+            return "как динозавр";
+        }
+    }
+
+    public static class SmallAnimal {
+        String getSize() {
+            return "как кошка";
+        }
+    }
+
+    public static class Goose {
+
+    }
+
+    public static class Dragon {
+
+    }
+}
